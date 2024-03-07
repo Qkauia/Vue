0c15f2de70cf5996426d1c9fffb17dd94cc1f952
艸先生 <kauia96@gmail.com>
2024-03-07 16:10:15 +0800

    Vue樣版語法基本練習

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,4 @@
-<script setup>
+<!-- <script setup>
 console.log(123);
 </script>
 
@@ -12,4 +12,60 @@ console.log(123);
 main {
   background-color: green;
 }
+</style> -->
+
+<!-- 嵌入文字 -->
+<!-- <script setup>
+let message = "Hello, Vue!";
+</script>
+<template>
+  <div>{{ message }}</div>
+</template>
+<style scoped></style> -->
+
+<!-- 嵌入 HTML標籤 -->
+<!-- <script setup>
+let message = "<u>Hello, Vue的HTML語法</u>";
+</script>
+<template>
+  <div v-html="message"></div>
+</template>
+<style scoped></style> -->
+
+<!-- 嵌入標籤屬性帶入變數 -->
+<!-- <script setup>
+  let message = "Hello-Vue";
+  let name = "content";
+  let type = "module";
+</script>
+<template>
+  <div v-bind:id="type">第一個方法</div>
+  <div :class="name">操作標籤的屬性</div> -->
+<!-- 也可以在變數帶入JS語法(轉大寫) -->
+<!-- <div>{{ message.toUpperCase() }}</div>
+</template>
+<style scoped>
+  .content {
+    color: red;
+  }
+</style> -->
+
+<!-- 屬性添加JS語言判斷 -->
+<script setup>
+  let check = true;
+  let light = "白天";
+  let dark = "黑夜";
+</script>
+<template>
+  <div :class="check ? 'dark' : 'light'">{{ check ? dark : light }}</div>
+</template>
+<style scoped>
+  .dark {
+    background-color: black;
+    color: white;
+  }
+  .light {
+    background-color: yellow;
+    color: black;
+  }
 </style>
